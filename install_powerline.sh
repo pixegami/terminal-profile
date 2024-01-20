7e13b8661a10629ebfa1380af700ce31e53feccb
AKA717 <alwinaji717@gmail.com>
2024-01-20 21:10:46 +0530

    added the cutomization in powerline script to install the pip package in virtual env

--- install_powerline.sh
+++ install_powerline.sh
@@ -3,11 +3,21 @@ set -eux pipefail
 
 # Install Powerline for VIM.
 sudo apt install -y python3-pip
-pip3 install --user powerline-status
+python3 -m venv ~/.powerline-venv
+source ~/.powerline-venv/bin/activate
+pip install powerline-status
+
+if [ -f ~/.powerline-venv/bin/powerline-daemon ]; then
+	powerline-daemon -q
+	POWERLINE_BASH_CONTINUATION=1
+	POWERLINE_BASH_SELECT=1
+	. ~/.powerline-venv/lib/python3.*/site-packages/powerline/bindings/bash/powerline.sh
+fi
+
 sudo cp configs/.vimrc ~/.vimrc
 sudo apt install -y fonts-powerline
 
 # Install Patched Font
 mkdir ~/.fonts
-sudo cp -a fonts/. ~/.fonts/
+sudo cp fonts/. ~/.fonts/
 fc-cache -vf ~/.fonts/
